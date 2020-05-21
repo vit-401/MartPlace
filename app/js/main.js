13d7f00e76627d96cc1680ba4ac10a945cd1b13a
vit-401 <vetas-tektonik1@gmail.com>
2020-05-21 14:48:44 +0300

    added srction slider-suggest

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,24 @@
-$(function(){
+$(function () {
 
-$('.newProduct-link').on('click', function () {
-  $(this).toggleClass('active_btn');
-  $(this).siblings().removeClass('active_btn');
-});
+  $('.suggest__slider-inner').slick({
+    slidesToShow: 1,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+  })
 
+  $('.newProduct-link').on('click', function () {
+    $(this).toggleClass('active_btn');
+    $(this).siblings().removeClass('active_btn');
+  });
+
+  $(".rating-slide").rateYo({
+    rating: 4.5,
+    readOnly: true,
+    starWidth: "17px",
+    
+    
+  });
 
 
 });
